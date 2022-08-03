e0f17d10c9e177aa6ca3209f9c1c02d3dd0698b7
jeannotds <diambujeannot@gmail.com>
2022-08-03 15:05:52 +0100

    utilisation de reduce avec objet

--- script.js
+++ script.js
@@ -5,5 +5,16 @@
         return tableau.indexOf(3)
         // return som
   });
-
   console.log(v)
+
+
+//   #################### AvEC OBJOT ############################
+
+let tableaux = [{x: 1}, {x:2}, {x:3}]
+var valeurInitiale = 0;
+
+let somme = tableaux.reduce((acc, val)=>
+    acc+val.x, 0
+)
+
+console.log(somme)
